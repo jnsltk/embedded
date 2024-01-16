e78c57397b7bce4037aea377d73afff7ea835e3c
Henrik L <helagro@outlook.com>
2024-01-16 18:32:26 +0100

    Add wp0 e2.c and e3.c

--- wp0/ruirui/e2.c
+++ wp0/ruirui/e2.c
@@ -1,11 +1,10 @@
 // (C) Henrik Lagrosen, Shiyao Xin, János Litkei, group: 35 (2024)
 // Work package 0
-// Exercise 1
+// Exercise 2
 // Submission code: 
 #include <stdio.h>
 
 // Main function in the program, no program arguments supported
 int main(int argc, char *argv[]){
-    // Print a string to the console
-    printf("%s", "Hello World! \n");
+    printf("Hello World - I'm %s! \n", argv[1]);
 }
---
+++ wp0/ruirui/e3.c
@@ -0,0 +1,26 @@
+// (C) Henrik Lagrosen, Shiyao Xin, János Litkei, group: 35 (2024)
+// Work package 0
+// Exercise 3
+// Submission code: 
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc == 1){
+        printf("No argument provided! \n");
+    } else if(argc > 2){
+        printf("Too many arguments provided! \n");
+    }
+    int bigEnoughFirstArg = (argc >= 2 && sizeof(&argv[1]) / sizeof(char) >= 2);
+    int isHelp = (bigEnoughFirstArg && argv[1][0] == '-' && argv[1][1] == 'h'); 
+    if(argc != 2 || isHelp){
+        printf(
+            "Example usage: %s %s\nTo get help: %s -h\n", 
+            argv[0], 
+            "\"Me\"",
+            argv[0]
+        );
+        return 0;
+    }
+
+    printf("Hello World - I'm %s! \n", argv[1]);
+}
