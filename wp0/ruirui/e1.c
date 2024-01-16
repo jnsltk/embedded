d39601f115020f270c29de763a52ba22c03c06d8
Henrik L <helagro@outlook.com>
2024-01-16 14:42:24 +0100

    Add wp0 e1.c

---
+++ wp0/ruirui/e1.c
@@ -0,0 +1,11 @@
+// (C) Henrik Lagrosen, Shiyao Xin, János Litkei, group: 35 (2024)
+// Work package 0
+// Exercise 1
+// Submission code: 
+#include <stdio.h>
+
+// Main function in the program, no program arguments supported
+int main (void) {
+    // Print a string to the console
+    printf("%s", "Hello World! \n");
+}
