6c8b7ce147f9cdad02c04ba5d51804ac98a10eb5
Henrik L <helagro@outlook.com>
2024-01-22 16:05:14 +0100

    Update clang again

---
+++ wp1/ruirui/e1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define s0 "In the beginning God created the heavens and the earth."
+#define s1                                                                                         \
+    "Now the earth was formless and empty, darkness was over the surface of the deep, and the "    \
+    "Spirit of God was hovering over the waters."
+#define s2 "And God said, \" Let there be light, \" and there was light."
+#define s3 "God saw that the light was good, and he separated the light from the darkness."
+#define
+
+const char **sentences = {
+    , "God called the light \"day,\" and the darkness he called \"night.\" And there was evening, "
+      "and there was morning—the first day."};
+
+int main() {
+    printf("%s", sentences[2]);
+    return 0;
+}
