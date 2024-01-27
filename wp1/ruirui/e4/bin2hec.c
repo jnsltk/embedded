335143241c1cc6bb42ee75be35c5983b97bb95b2
Henrik L <helagro@outlook.com>
2024-01-27 12:09:21 +0100

    Add bin2hec

--- wp1/ruirui/e4/bin2hec.c
+++ wp1/ruirui/e4/bin2hec.c
@@ -1,3 +1,71 @@
+#include <limits.h>
 #include <stdio.h>
 
-int main() { return 0; }
+char getNumber(unsigned long *output);
+void printAsHex(unsigned long *input, const unsigned char size);
+
+int main() {
+    unsigned long input = 0;
+    if (!getNumber(&input)) {
+        fprintf(stderr, "Invalid number\n");
+        return 2;
+    }
+
+    if (input <= UCHAR_MAX) {
+        unsigned char inputChar = input;
+        printAsHex(((unsigned long *) &inputChar), sizeof(unsigned char));
+    } else if (input <= USHRT_MAX) {
+        unsigned short inputShort = input;
+        printAsHex(((unsigned long *) &inputShort), sizeof(unsigned short));
+    } else if (input <= UINT_MAX) {
+        unsigned int inputInt = input;
+        printAsHex(((unsigned long *) &inputInt), sizeof(unsigned int));
+    } else {
+        printAsHex(&input, sizeof(unsigned long));
+    }
+
+    return 0;
+}
+
+// defines function which reads a number in range from sdin
+char getNumber(unsigned long *output) {
+
+    char inputStr[70];   // reserves char array used to temporarily store the string the user enters
+    fgets(inputStr, sizeof(inputStr), stdin);   // reads a string from stdin with
+
+    char *c = inputStr;
+    unsigned long mask = (1UL << (sizeof(unsigned long) * 8UL - 1UL));
+
+    do {
+        if (*c == '1') {
+            *output |= mask;
+        }
+
+        mask >>= 1;
+        c++;
+
+        if (mask == 0)
+            return 0;
+    } while (*c != '\n');   // while the string has characters
+
+    while (mask != 0) {
+        mask >>= 1;
+        *output >>= 1;
+    }
+
+    return 1;
+}
+
+void printAsHex(unsigned long *input, const unsigned char size) {
+    for (int i = size * 2 - 4; i >= 0; i--) {
+        const unsigned long mask = 0b1111UL << (i * 4);
+        const unsigned char output = (*input & mask) >> (i * 4);
+
+        if (output < 10)
+            printf("%hhu", output);
+        else
+            printf("%c", output + 55);
+    }
+
+    printf("\n");
+}
--- wp1/ruirui/e4/dec2bin.c
+++ wp1/ruirui/e4/dec2bin.c
@@ -1,27 +1,66 @@
+#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>   // includes stdlib for strtol
+#include <stdlib.h>   // includes stdlib for
 
-int getNumber();
+char getNumber(unsigned long *output);
+void printAsBin(unsigned long *input, unsigned char size);
 
-int main() {
-    const int input = getNumber();
+int main(const int argc, char **argv) {
+    if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'h') {
+        printf("This is a program that converts numbers to binary. Please run without -h and enter "
+               "any positive whole number\n");
+        return 0;
+    }
 
-    printf("%d", input);
+    unsigned long input;
+    if (!getNumber(&input)) {
+        fprintf(stderr, "Invalid number\n");
+        return 2;
+    }
+
+    if (input <= UCHAR_MAX) {
+        unsigned char inputChar = input;
+        printAsBin(((unsigned long *) &inputChar), sizeof(unsigned char));
+    } else if (input <= USHRT_MAX) {
+        unsigned short inputShort = input;
+        printAsBin(((unsigned long *) &inputShort), sizeof(unsigned short));
+    } else if (input <= UINT_MAX) {
+        unsigned int inputInt = input;
+        printAsBin(((unsigned long *) &inputInt), sizeof(unsigned int));
+    } else {
+        printAsBin(&input, sizeof(unsigned long));
+    }
+
     return 0;
 }
 
-int getNumber() {        // defines function which reads a number in range from sdin
-    char inputStr[11];   // reserves char array used to temporarily store the string the user enters
+// defines function which reads a number in range from sdin
+char getNumber(unsigned long *output) {
+
+    char inputStr[30];   // reserves char array used to temporarily store the string the user enters
     fgets(inputStr, sizeof(inputStr), stdin);   // reads a string from stdin with
 
     char *endptr;   // defines pointer wich will point to where the first argument stopped parsing
-    const long guessInt = strtol(inputStr, &endptr, 10);   // gets long version of first argument
+    *output = strtoul(inputStr, &endptr, 10);   // gets long version of first argument
+    if (*output == ULLONG_MAX)
+        return 0;
 
     // if the number is valid
-    if (*endptr == '\n')
-        return guessInt;   // return entered argument as a number
-    else {                 // if the number is not valid
-        fprintf(stderr, "Invalid number");
-        return -1;
+    return *endptr == '\n';
+}
+
+void printAsBin(unsigned long *input, const unsigned char size) {
+    const char bitAmt = size * 8;
+    const unsigned long filter = (1UL << (bitAmt - 1UL));
+
+    // loops through the bits of the result number
+    for (int i = 0; i < bitAmt; i++) {
+        // essentially gets the currently most significant bit of n
+        const unsigned char bit = (*input & filter) != 0UL;
+
+        printf("%hhu", bit);
+        *input <<= 1;   // shift n one step to the left
     }
+
+    printf("\n");
 }
