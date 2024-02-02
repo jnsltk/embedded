bc375bd6ff30637b85f6e5f964ae8268a3d84ab3
Henrik L <helagro@outlook.com>
2024-02-02 18:27:09 +0100

    Add wp2

---
+++ wp2/ruirui/e5/README.md
@@ -0,0 +1,1 @@
+gcc decode.c -o decode && ./decode $(./code 1 7 3 1 1 | ./bin2hex)
--- wp2/ruirui/e5/decode.c
+++ wp2/ruirui/e5/decode.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
-#define TO_HEX -'A' + 10
-
 int main(int argc, char **argv) {
     if (argc != 2)
         return fprintf(stderr, "1 argument expected\n");
 
-    char *c = argv[1];
-    unsigned char n = 0;
+    char n = 0, *c = argv[1];
 
     do {
         n <<= 4;
 
         if (*c >= 'A' && *c <= 'F')
-            n |= (*c + TO_HEX);
+            n |= (*c - 55);
         else if (*c >= '0' && *c <= '9')
             n |= (*c - '0');
         else
-            return fprintf(stderr, "Invalid input: %c\n", *c);
+            return fprintf(stderr, "Invalid input\n");
     } while (*(++c) != '\0');
-    printf("%hhu\n", n);
+
+    printf("Name          Value\n---------------------\n"
+           "engine_on       %hhu\n"
+           "gear_pos        %hhu\n"
+           "key_pos         %hhu\n"
+           "break1          %hhu\n"
+           "break2          %hhu\n",
+           (0b10000000 & n) >> 7, (0b1110000 & n) >> 4, (0b1100 & n) >> 2, (0b10 & n) >> 1,
+           0b1 & n);
 }
