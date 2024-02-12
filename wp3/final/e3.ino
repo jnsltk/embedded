289c7378cf38d8d508eeb85853e2f72a192aa27b
Henrik L <helagro@outlook.com>
2024-02-12 16:43:49 +0100

    unspecified

--- wp3/final/e3.ino
+++ wp3/final/e3.ino
@@ -1,16 +1,19 @@
 
+// defines setuo function which runs once when program start
 void setup()
 {
-  Serial.begin(9600);
+  // initialises the serial communication at 9600 bits per second
+  Serial.begin(9600); 
 }
 
+// defines loop function which runs continuously
 void loop()
 {  
-  const int tmpRead = analogRead(A0);
-  const int tmpVol = map(tmpRead, 0, 1023, 0, 5000);
-  const int tmp = (tmpVol - 500) / 10;
+  const int tmpRead = analogRead(A0); // reads the value from the temperature sensor
+  const int tmpVol = map(tmpRead, 0, 1023, 0, 5000); // converts the value to voltage
+  const int tmp = (tmpVol - 500) / 10; // converts the voltage to temperature in Celsius
 
-  Serial.println(tmp);
+  Serial.println(tmp); // prints the temperature to the serial monitor
 
-  delay(500);
+  delay(500); // delays for 0.5 seconds
 }
