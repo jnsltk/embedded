f836de0ea586dfb89fb7a1da98e8ae84eaae06dc
jnsltk <janos.litkei@gmail.com>
2024-02-12 21:40:13 +0100

    add comments

--- wp3/final/e1.ino
+++ wp3/final/e1.ino
@@ -1,36 +1,36 @@
-const int led1 = 2;
-const int led2 = 3;
-const int button = 4;
+const int led1 = 2; // define the pin for the LED 1 (pin 2)
+const int led2 = 3; // define the pin for the LED 2 (pin 3)
+const int button = 4; // define the pin for the button (pin 4)
 
-int buttonState = 0;
-unsigned long timeNow = 0;
+int buttonState = 0; // define the variable for the button state
+unsigned long timeNow = 0; // define the variable for the time
 
-void setup()
+void setup() // setup function (runs once)
 {
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-    pinMode(button, INPUT);
+    pinMode(led1, OUTPUT); // set the LED 1 pin as an output
+    pinMode(led2, OUTPUT); // set the LED 2 pin as an output
+    pinMode(button, INPUT); // set the button pin as an input
 
-    timeNow = millis();
+    timeNow = millis(); // set the time to the current time
 
 }
 
-void loop()
+void loop() // loop function (runs over and over again)
 {
-    buttonState = digitalRead(button);
+    buttonState = digitalRead(button); // read the state of the button
 
-    if (buttonState == HIGH) {
-        digitalWrite(led2, HIGH);
-    } else {
-        digitalWrite(led2, LOW);
+    if (buttonState == HIGH) { // if the button is pressed
+        digitalWrite(led2, HIGH); // turn on the LED 2
+    } else { // if the button is not pressed
+        digitalWrite(led2, LOW); // turn off the LED 2
     }
 
-    if (timeNow + 1000 == millis()) {
-        digitalWrite(led1, HIGH);
+    if (timeNow + 1000 == millis()) { // if 1 second has passed
+        digitalWrite(led1, HIGH); // turn on the LED 1 
     }
 
-    if (timeNow + 2000 == millis()) {
-        digitalWrite(led1, LOW);
-        timeNow = millis();
+    if (timeNow + 2000 == millis()) { // if 2 seconds have passed
+        digitalWrite(led1, LOW); // turn off the LED 1
+        timeNow = millis(); // reset the time
     }
 }
