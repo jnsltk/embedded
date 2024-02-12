3bacde32545a9dc927984d1a060149f6b4986e90
shiyaox <shiyaox@student.chalmers.se>
2024-02-12 17:39:03 +0100

    Add comments

--- wp3/final/e2.ino
+++ wp3/final/e2.ino
@@ -1,74 +1,65 @@
 // C++ code
 //
-#define TEMP A0
+/* ------------------------------ Define macros ----------------------------- */
+#define TEMP         A0
 #define LIGHT_SENSOR A1
-#define YELLOWLED 2
-#define REDLED 3
-#define GREENLED 4
+#define YELLOWLED    2
+#define REDLED       3
+#define GREENLED     4
 
-void setup()
-{
-  pinMode(GREENLED, OUTPUT); // configure the green LED
-  pinMode(YELLOWLED, OUTPUT); // configure the yellow LED
-  pinMode(REDLED, OUTPUT); // configure the red LED
-  Serial.begin(9600); // initialize the serial communication
+// defines setuo function which runs once when program start
+void setup() {
+    pinMode(GREENLED, OUTPUT);    // configure the green LED
+    pinMode(YELLOWLED, OUTPUT);   // configure the yellow LED
+    pinMode(REDLED, OUTPUT);      // configure the red LED
+    Serial.begin(9600);           // initialize the serial communication
 }
 
-void loop()
-{
-  double tempValue = analogRead(TEMP); // retrieve the value from temperature sensor
-  // map the read in values to the actual temperature values in farenheight
-  double temperature = map(tempValue, 20, 358, -40, 125);
-  // retrieve the value of the light sensor 
-  double lightValue = analogRead(LIGHT_SENSOR);
-  // map the read in values in percentage
-  double lightIntensity = map(lightValue, 0, 13, 0, 100);
-  // light up the LED	
-  indicate_led(temperature, lightIntensity);
-  
-  Serial.print("Temp:");
-  Serial.print(temperature);
-  Serial.print("C\t");
-  Serial.print("Light:");
-  Serial.print(lightIntensity);
-  Serial.println("%");
-  delay(1000); // Wait for 1000 millisecond(s)
+// defines loop function which runs continuously
+void loop() {
+    double tempValue = analogRead(TEMP);   // retrieve the value from temperature sensor
+    // map the read in values to the actual temperature values in farenheight
+    double temperature = map(tempValue, 20, 358, -40, 125);
+    // retrieve the value of the light sensor
+    double lightValue = analogRead(LIGHT_SENSOR);
+    // map the read in values in percentage
+    double lightIntensity = map(lightValue, 0, 13, 0, 100);
+    // light up the LED
+    indicate_led(temperature, lightIntensity);
+
+    Serial.print("Temp:");
+    Serial.print(temperature);
+    Serial.print("C\t");
+    Serial.print("Light:");
+    Serial.print(lightIntensity);
+    Serial.println("%");
+    delay(1000);   // Wait for 1000 millisecond(s)
 }
 
 // This function compare the values of the temperature sensor and light sensor, and light up the LED
-void indicate_led(double temperature, double lightIntensity)
-{
-  // indicate the normal dependency
-  if ((temperature < -12 && lightIntensity == 0) || 
-    (temperature >= -12 && temperature <=0 && 
-     lightIntensity >= 1 && lightIntensity <= 20) || 
-    (temperature >= 0 && temperature <= 20 && 
-     lightIntensity >= 21 && lightIntensity <= 60) ||
-    (temperature >= 21 && lightIntensity >= 61 && 
-     lightIntensity <= 100))
-  {
-   	 digitalWrite(GREENLED, HIGH); // turn on green
-     digitalWrite(YELLOWLED, LOW); // turn off yellow
-     digitalWrite(REDLED, LOW); // turn off red
-  } // deviation when the temperature is higher  
-  else if ((temperature >= -12 && lightIntensity == 0) || 
-    (temperature > 0 && 
-     lightIntensity >= 1 && lightIntensity <= 20) || 
-    (temperature > 20 && 
-     lightIntensity >= 21 && lightIntensity <= 60))
-  {
-   	 digitalWrite(REDLED, HIGH); // turn on red
-     digitalWrite(YELLOWLED, LOW); // turn off yellow
-     digitalWrite(GREENLED, LOW); // turn off green
-  } //  deviation when the temperature is lower than it should be  
-  else if ((temperature < -12 && temperature <=0 && 
-     lightIntensity >= 1 && lightIntensity <= 20) || 
-    (temperature < 0 && lightIntensity >= 21 && 
-     lightIntensity <= 60) ||
-    (temperature < 21 && lightIntensity <= 100))
-  {
-     digitalWrite(YELLOWLED, HIGH); // turn on yellow
-     digitalWrite(REDLED, LOW); // turn off red
-     digitalWrite(GREENLED, LOW); // turn off green
-  }
+void indicate_led(double temperature, double lightIntensity) {
+    // indicate the normal dependency
+    if ((temperature < -12 && lightIntensity == 0) ||
+        (temperature >= -12 && temperature <= 0 && lightIntensity >= 1 && lightIntensity <= 20) ||
+        (temperature >= 0 && temperature <= 20 && lightIntensity >= 21 && lightIntensity <= 60) ||
+        (temperature >= 21 && lightIntensity >= 61 && lightIntensity <= 100)) {
+        digitalWrite(GREENLED, HIGH);   // turn on green
+        digitalWrite(YELLOWLED, LOW);   // turn off yellow
+        digitalWrite(REDLED, LOW);      // turn off red
+    }                                   // deviation when the temperature is higher
+    else if ((temperature >= -12 && lightIntensity == 0) ||
+             (temperature > 0 && lightIntensity >= 1 && lightIntensity <= 20) ||
+             (temperature > 20 && lightIntensity >= 21 && lightIntensity <= 60)) {
+        digitalWrite(REDLED, HIGH);     // turn on red
+        digitalWrite(YELLOWLED, LOW);   // turn off yellow
+        digitalWrite(GREENLED, LOW);    // turn off green
+    }   //  deviation when the temperature is lower than it should be
+    else if ((temperature < -12 && temperature <= 0 && lightIntensity >= 1 &&
+              lightIntensity <= 20) ||
+             (temperature < 0 && lightIntensity >= 21 && lightIntensity <= 60) ||
+             (temperature < 21 && lightIntensity <= 100)) {
+        digitalWrite(YELLOWLED, HIGH);   // turn on yellow
+        digitalWrite(REDLED, LOW);       // turn off red
+        digitalWrite(GREENLED, LOW);     // turn off green
+    }
 }
--- wp3/final/e4.ino
+++ wp3/final/e4.ino
@@ -1,46 +1,40 @@
 // C++ code
 //
-#include <Keypad.h>
+#include <Keypad.h>   // include the library
 
-#define ROWS 4
-#define COLS 4
+#define ROWS 4   // define ROWS as 4
+#define COLS 4   // define COLS as 4
 
 // use an array to store key values on the keypad
-char keys[ROWS][COLS] = { 
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
+char keys[ROWS][COLS] = {
+    {'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
 
-byte rowPins[ROWS] = {11, 10, 9, 8}; // connect to Arduino pins
-byte colPins[COLS] = {7, 6, 5, 4}; // connect to Arduino pins
+byte rowPins[ROWS] = {11, 10, 9, 8};   // connect to Arduino pins
+byte colPins[COLS] = {7, 6, 5, 4};     // connect to Arduino pins
 
 // create a keypad object
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-void setup()
-{
-  //ROWS -> outport 0
-  //COLS -> inport 1
-  
-  Serial.begin(9600);
+
+// defines setuo function which runs once when program start
+void setup() {
+    Serial.begin(9600);   // initialize the serial communication
 }
 
-void loop()
-{
-  char pressedKey = readKey(); // store the pressed key value in a variable
-  if (pressedKey != 0) { // check whether a key got pressed
-  	// Print the pressed key value
-  	Serial.println(pressedKey);
-    delay(100); // Wait for 1000 millisecond(s)
-  }
+// defines loop function which runs continuously
+void loop() {
+    char pressedKey = readKey();   // store the pressed key value in a variable
+    if (pressedKey != 0) {         // check whether a key got pressed
+        // Print the pressed key value
+        Serial.println(pressedKey);
+        delay(100);   // Wait for 1000 millisecond(s)
+    }
 }
 
 // This function checks if a key being pressed, and return the key value
 char readKey() {
-  char pressedKey = keypad.getKey(); // get the pressed key value
-  if(pressedKey) { // check if a key is pressed
-    return pressedKey; // if yes, return the key
-  }
-  return 0; // otherwise return 0
+    char pressedKey = keypad.getKey();   // get the pressed key value
+    if (pressedKey) {                    // check if a key is pressed
+        return pressedKey;               // if yes, return the key
+    }
+    return 0;   // otherwise return 0
 }
