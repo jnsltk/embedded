394e1b0444bde02bb185b03ae49a313a2579eeae
Henrik L <helagro@outlook.com>
2024-02-12 16:27:38 +0100

    unspecified

--- wp3/ruirui/e6.c
+++ wp3/ruirui/e6.c
@@ -1,23 +1,27 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h>    // includes standard i/o library
+#include <string.h>   // includes string library for strcpy
 
-#define MAX_INPUT 20
+#define MAX_INPUT 21   // defines the maximum input length
 
+// custom function to copy a string
 void copyString(char *dest, char *src) {
-    do {
-        *dest = *src;
-        dest++;
+    do {                // do-while loop to copy the string
+        *dest = *src;   // copies the character from src to dest
+        dest++;         // increments the destination pointer
+        // increments the source pointer and checks if it's the end of the string
     } while (*(++src) != '\0');
 
-    *dest = '\0';
+    *dest = '\0';   // adds the null terminator to the end of the string
 }
 
+// main function
 int main() {
+    // declares the original array and the two copies
     char input[MAX_INPUT], c1[MAX_INPUT], c2[MAX_INPUT];
-    char *end = fgets(input, MAX_INPUT, stdin);
+    fgets(input, MAX_INPUT, stdin);   // reads string from stdin
 
-    strcpy(c1, input);
-    copyString(c2, input);
+    strcpy(c1, input);       // copies the string using strcpy
+    copyString(c2, input);   // copies the string using my own brilliant function
 
     printf("Original:   %s\n", input);
     printf("strcpy:     %s\n", c1);
