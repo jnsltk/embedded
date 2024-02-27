e0ab4816bfdb7be9b1b35f65a80fb691ee29736c
Henrik L <helagro@outlook.com>
2024-02-27 14:14:07 +0100

    Add comment

--- wp5/final/e1/e1.ino
+++ wp5/final/e1/e1.ino
@@ -1,32 +1,40 @@
-#define RED_LED 4
-#define YELLOW_LED 3
-#define SWITCH 2
+// (C) Henrik Lagrosen, János Litkei, Shiyao Xin; Group 35 (2024)
+// Work package 5
+// Exercise 1
+// Submission code: 662248
 
-bool y_led_on = false;
+#define RED_LED 4 // defines pin for red LED
+#define YELLOW_LED 3 // defines pin for yellow LED
+#define SWITCH 2 // defines pin for switch
 
-void isr();
+bool y_led_on = false; // flag to keep track of yellow LED state
 
+void isr(); // declares function for interrupt service routine
+
+// defines setup function
 void setup() {
-    pinMode(RED_LED, OUTPUT);
-    pinMode(YELLOW_LED, OUTPUT);
-    pinMode(SWITCH, INPUT);
+    pinMode(RED_LED, OUTPUT); // sets red LED pin as output
+    pinMode(YELLOW_LED, OUTPUT); // sets yellow LED pin as output
+    pinMode(SWITCH, INPUT); // sets switch pin as input
 
-    attachInterrupt(digitalPinToInterrupt(SWITCH), isr, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(SWITCH), isr, CHANGE); // attaches interrupt to switch pin
 }
 
+// defines loop function
 void loop() {
-    digitalWrite(RED_LED, HIGH);
-    delay(1000);
-    digitalWrite(RED_LED, LOW);
-    delay(1000);
+    digitalWrite(RED_LED, HIGH);  // turns on red LED
+    delay(1000); // delays for 1 second
+    digitalWrite(RED_LED, LOW); // turns off red LED
+    delay(1000); // delays for 1 second
 }
 
+// defines interrupt service routine
 void isr() {
-    if (y_led_on) {
-        digitalWrite(YELLOW_LED, LOW);
-        y_led_on = false;
-    } else {
-        digitalWrite(YELLOW_LED, HIGH);
-        y_led_on = true;
+    if (y_led_on) { // if yellow LED is on
+        digitalWrite(YELLOW_LED, LOW); // turns off yellow LED
+        y_led_on = false; // updates flag
+    } else { // if yellow LED is off
+        digitalWrite(YELLOW_LED, HIGH); // turns on yellow LED
+        y_led_on = true; // updates flag
     }
 }
